1b4b0078ce8d1767add7828fb293e7080598fc97
gussrivero <gustavo.rivero@mercadolibre.com>
2021-12-20 15:09:11 -0300

    Creation up to exercise 5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,29 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    println("Integrador Kotlin")
+
+
+    val car = Vehicle("AA111AA",VehicleType.CAR, Calendar.getInstance(),"DISCOUNT_CARD_001")
+    val moto = Vehicle("AA222AA",VehicleType.MOTORCYCLE, Calendar.getInstance())
+    val minibus  = Vehicle("AA333AA",VehicleType.MINIBUS, Calendar.getInstance())
+    val bus = Vehicle("AA444AA",VehicleType.BUS, Calendar.getInstance(),"DISCOUNT_CARD_002")
+
+    val parking = Parking(mutableSetOf(car,moto,minibus,bus))
+
+    println(parking.vehicle.contains(car))
+    println(parking.vehicle.contains(moto))
+    println(parking.vehicle.contains(minibus))
+    println(parking.vehicle.contains(bus))
+
+
+    val car2 = Vehicle("AA111AA",VehicleType.CAR, Calendar.getInstance(),"DISCOUNT_CARD_001")
+
+    val isCard2Inserted = parking.vehicle.add(car2)
+    println(isCard2Inserted)
+
+
+
+    //Consigna 1
+    //Se define Vehicles como un set porque esta estructura no permite repetir dentro de su collecion
 }
