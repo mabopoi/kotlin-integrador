1768c954734902691564fc2e8037c6f24a96d37e
mabopoi <marco.bottino@mercadolibre.com>
2021-12-23 09:15:36 -0300

    Add test cases

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,11 +6,22 @@ fun main(args: Array<String>) {
     //Exercise 1
     //Se define Vehicles como un set porque esta estructura no permite repetir dentro de su collecion
 
-    val car = Vehicle("AA111AA",VehicleType.CAR, Calendar.getInstance())
+    val testCalendarCar = Calendar.getInstance()
+    testCalendarCar.add(Calendar.HOUR, -3)
+    val testCalendarMotorcycle = Calendar.getInstance()
+    testCalendarMotorcycle .add(Calendar.HOUR, -2)
+    testCalendarMotorcycle .add(Calendar.MINUTE, -12)
+    val testCalendarMinibus = Calendar.getInstance()
+    testCalendarMinibus.add(Calendar.HOUR, -1)
+    val testCalendarBus = Calendar.getInstance()
+    testCalendarBus.add(Calendar.HOUR, -3)
+    testCalendarBus.add(Calendar.MINUTE, -49)
+
+    val car = Vehicle("AA111AA",VehicleType.CAR, testCalendarCar)
     val car2 = Vehicle("AA111AA1",VehicleType.CAR, Calendar.getInstance(),"DISCOUNT_CARD_001")
-    val moto = Vehicle("AA222AA",VehicleType.MOTORCYCLE, Calendar.getInstance())
-    val minibus  = Vehicle("AA333AA",VehicleType.MINIBUS, Calendar.getInstance())
-    val bus = Vehicle("AA444AAA",VehicleType.BUS, Calendar.getInstance(),"DISCOUNT_CARD_002")
+    val motorcycle = Vehicle("AA222AA",VehicleType.MOTORCYCLE, testCalendarMotorcycle)
+    val minibus  = Vehicle("AA333AA",VehicleType.MINIBUS, testCalendarMinibus)
+    val bus = Vehicle("AA444AAA",VehicleType.BUS, testCalendarBus ,"DISCOUNT_CARD_002")
     val bus2 = Vehicle("AA444AAB",VehicleType.BUS, Calendar.getInstance(),"DISCOUNT_CARD_002")
     val bus3 = Vehicle("AA444AAC",VehicleType.BUS, Calendar.getInstance(),"DISCOUNT_CARD_002")
     val bus4 = Vehicle("AA444AAD",VehicleType.BUS, Calendar.getInstance(),"DISCOUNT_CARD_002")
@@ -31,7 +42,7 @@ fun main(args: Array<String>) {
     val parking = Parking(mutableSetOf(), 20, Pair(0,0))
 
     //Exercise 6
-    val vehiclesList = listOf(car,car2,moto, minibus, bus, bus2, bus3, bus4, bus5, bus6,bus7,bus8, bus9,
+    val vehiclesList = listOf(car,car2,motorcycle, minibus, bus, bus2, bus3, bus4, bus5, bus6,bus7,bus8, bus9,
         bus10, bus11, bus12, bus13, bus14, bus15, bus16, bus17)
 
     vehiclesList.forEach {
@@ -40,8 +51,21 @@ fun main(args: Array<String>) {
         else println("Sorry, the check-in failed")
     }
 
-    val parkingSpace = ParkingSpace(car, parking)
-    parkingSpace.checkOutVehicle("AA111AA")
+    val parkingSpaceCar = ParkingSpace(car, parking)
+    val parkingSpaceMotorcycle = ParkingSpace(motorcycle, parking)
+    val parkingSpaceMinibus = ParkingSpace(minibus, parking)
+    val parkingSpaceBus = ParkingSpace(bus, parking)
+    val testParkingSpace = ParkingSpace(Vehicle("testPlate", VehicleType.CAR, Calendar.getInstance()), parking)
+
+    parkingSpaceCar.checkOutVehicle("AA111AA1") //fails because plate does not match
+    parkingSpaceCar.checkOutVehicle("AA111AA")
+    parkingSpaceMotorcycle.checkOutVehicle("AA222AA")
+    parkingSpaceMinibus.checkOutVehicle("AA333AA")
+    parkingSpaceBus.checkOutVehicle("AA444AAA")
+    testParkingSpace.checkOutVehicle("testPlate") //fails because the plate cant be found in the parking
+
+    //prints false because a vehicle with the same plate already exists
+    println(parking.addVehicle(Vehicle("AA444AAB",VehicleType.MOTORCYCLE, Calendar.getInstance())))
 
     parking.showResults()
     parking.listVehicles()
